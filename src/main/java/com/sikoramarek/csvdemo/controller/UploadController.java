0c02be085ebf8bc001c51508c8ae8418292ebe41
Marek <maretzky85@gmail.com>
2019-10-19 22:21:38 +0200

    Added ExceptionHandler

--- src/main/java/com/sikoramarek/csvdemo/controller/UploadController.java
+++ src/main/java/com/sikoramarek/csvdemo/controller/UploadController.java
@@ -2,10 +2,16 @@ package com.sikoramarek.csvdemo.controller;
 
 import com.sikoramarek.csvdemo.service.UploadService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
 
 @RestController
 public class UploadController {
@@ -19,6 +25,11 @@ public class UploadController {
 	@PostMapping("upload")
 	public ResponseEntity upload(@RequestParam("file") MultipartFile multipartFile) {
 		return uploadService.parseFile(multipartFile);
+	}
+
+	@ExceptionHandler(MultipartException.class)
+	ResponseEntity<String> handleBadRequest(HttpServletRequest req, Exception ex) {
+		return new ResponseEntity<>(ex.getMessage(), NOT_ACCEPTABLE);
 	}
 
 }
